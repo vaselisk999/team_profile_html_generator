00a0f54f20129cff93796076d1e595711bb4e0c8
ArturK <vaselisk999@gmail.com>
2023-02-24 19:39:37 +0000

    prompt object structure

--- index.js
+++ index.js
@@ -13,16 +13,38 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-inquirer.prompt([{
-    //manager questions
-}]).then(response => {
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is manager name?',
+        name: 'username',
+    },
+    {
+        type: 'password',
+        message: 'What is manager ID?',
+        name: 'password',
+    },
+    {
+        type: 'password',
+        message: 'What is manager Email address?',
+        name: 'confirm',
+    },
+    {
+        type: 'password',
+        message: 'What is manager Office number?',
+        name: 'confirm',
+    },
+]).then(response => {
     // populate manager info
     // promptForNexEmployee ()
 })
 
 const promptForNextEmployee = () => {
     inquirer.prompt([{
-        // choice of 3
+        type: 'list',
+        message: 'Do you whant to add another Employee?',
+        name: 'confirm',
+        choices: ["engineer", "intern", "generate the team"],
     }]).then(response => {
         // if engineer
         //    promptForEngineer
@@ -34,23 +56,61 @@ const promptForNextEmployee = () => {
 }
 
 const promptForEngineer = () => {
-    inquirer.prompt([{
-        //engineer questions
-    }]).then(response => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is engineer name?',
+            name: 'username',
+        },
+        {
+            type: 'password',
+            message: 'What is engineer ID?',
+            name: 'password',
+        },
+        {
+            type: 'password',
+            message: 'What is engineer Email address?',
+            name: 'confirm',
+        },
+        {
+            type: 'password',
+            message: 'What is engineer GitHub username?',
+            name: 'confirm',
+        },
+    ]).then(response => {
         // add new engineer to employees array
         // promptForNextEmployee
     })
 }
 
 const promptForIntern = () => {
-    inquirer.prompt([{
-        //intern questions
-    }]).then(response => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is intern name?',
+            name: 'username',
+        },
+        {
+            type: 'password',
+            message: 'What is intern ID?',
+            name: 'password',
+        },
+        {
+            type: 'password',
+            message: 'What is intern Email address?',
+            name: 'confirm',
+        },
+        {
+            type: 'password',
+            message: 'What is intern School?',
+            name: 'confirm',
+        },
+    ]).then(response => {
         // add new intern to employees array
         // promptForNextEmployee
     })
 }
 
 const buildPage = () => {
-// render(myArrayOfTeamMembers)
+    // render(myArrayOfTeamMembers)
 }
